b59b5be2aee315664884768ddee120666b35e0f6
SM Mashuque <smjim1998@gmail.com>
2018-10-12 02:02:40 -0400

    added the stock symbols to the existing models.

--- fortune_1000/models.py
+++ fortune_1000/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=75)
+    stock_ticker = models.CharField(max_length=20, null=True)
     rank = models.IntegerField(null=True)
     revenue = models.IntegerField(null=True)
     profits = models.DecimalField(max_digits=20, decimal_places=3, null=True)
--- fortune_1000/views.py
+++ fortune_1000/views.py
@@ -6,3 +6,11 @@ from .models import Company
 class CompanyListView(ListView):
     template_name = "companies_list.html"
     model = Company
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        print("context:", context)
+        return context
+
+
+

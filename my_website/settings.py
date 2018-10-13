1b477e051c1bbad27c3e2d1f7116860c58c7b9ba
SM Mashuque <smjim1998@gmail.com>
2018-10-12 21:04:42 -0400

    fix heroku cred

--- my_website/settings.py
+++ my_website/settings.py
@@ -161,3 +161,4 @@ django_heroku.settings(locals())
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'

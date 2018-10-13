6b6c66ab9861b5871a5720117b24c922a489d686
SM Mashuque <smjim1998@gmail.com>
2018-10-12 21:13:50 -0400

    fix heroku cred

--- my_website/settings.py
+++ my_website/settings.py
@@ -104,10 +104,10 @@ WSGI_APPLICATION = 'my_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'smmashuque',
-        'USER': 'smmashuque',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'd8k3af1ao42qla',
+        'USER': 'jeceznlzplypbj',
+        'PASSWORD': '66e80a621183c39b942eca594557fff63430516b08bce9bd2a3694393003a9da',
+        'HOST': 'ec2-54-225-110-152.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -159,6 +159,6 @@ STATICFILES_DIRS = [
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'

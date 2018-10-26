d06ec2955b277fee50c1bdc83ce335a242120bbb
SM Mashuque <smjim1998@gmail.com>
2018-10-26 14:42:41 -0400

    fix deployment..

--- my_website/settings.py
+++ my_website/settings.py
@@ -166,6 +166,8 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), # needed for Django server
     os.path.join(BASE_DIR, "build/static"), # needed for React app

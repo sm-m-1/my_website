ce0cecde30736dda7b71e4b70b9ecf3cdbe6de0a
SM Mashuque <smjim1998@gmail.com>
2018-10-26 14:24:52 -0400

    add staticfiles dic in settings.

--- my_website/settings.py
+++ my_website/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), # needed for Django server

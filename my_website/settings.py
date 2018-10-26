66b4e05b778fa62be10f9ae20f5e6be78e289b35
SM Mashuque <smjim1998@gmail.com>
2018-10-26 14:55:56 -0400

    fix static file hosting on heroku

--- my_website/settings.py
+++ my_website/settings.py
@@ -63,7 +63,9 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     # 'google.com',
     # 'hostname.example.com',
+    # 'smmashuq.com/',
     'localhost:3000',
+    'localhost:8000',
 )
 
 
@@ -164,9 +166,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), # needed for Django server

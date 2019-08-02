c90f6bbc8dec94a7a77780fae1dedddf91710eb2
SM Mashuque <smjim1998@gmail.com>
2019-08-02 13:24:35 -0400

    make the graph hover better.

--- src/containers/CompanyDetailView.js
+++ src/containers/CompanyDetailView.js
@@ -94,22 +94,25 @@ class CompanyDetail extends React.Component {
       datasets: [
         {
           label: this.state.company.stock_symbol,
-          fill: false,
-          lineTension: 0.1,
+          fill: true,
           backgroundColor: 'rgba(75,192,192,0.4)',
           borderColor: 'rgba(75,192,192,1)',
           data: this.state.priceData,
-          pointBorderWidth: 0.5,
-          pointHoverRadius: 6,
+          pointHoverRadius: 5,
           pointHoverBackgroundColor: 'rgba(75,192,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointRadius: 0,
-          pointHitRadius: 3,
+          pointHitRadius: 5,
         }
       ]
     };
     var chartOptions = {
-      pointDot : false,
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+        titleFontSize: 15,
+        bodyFontSize: 15,
+      }
     };
 
     return (
